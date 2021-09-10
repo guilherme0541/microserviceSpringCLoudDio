59c3767dfad3fd4ec16955af47af0c7d90432b2f
Guilherme Silva <guilherme0541@gmail.com>
2021-09-10 17:48:42 -0300

    implemented service-discovery

--- service-discovery/src/main/java/com/guilherme/silva/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/guilherme/silva/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.guilherme.silva.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
